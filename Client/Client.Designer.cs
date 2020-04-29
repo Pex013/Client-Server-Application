eec70b95b97fbd6f8f3b1e5ae272d3bbfdb826c4
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-04-29 22:54:36 +0200

    Add Client Functionality

---
+++ Client/Client.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.backgroundpanel = new System.Windows.Forms.Panel();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.backgroundpanel.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnConnect.Location = new System.Drawing.Point(280, 100);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(94, 30);
+            this.btnConnect.TabIndex = 22;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel2.Location = new System.Drawing.Point(88, 124);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(140, 1);
+            this.panel2.TabIndex = 21;
+            // 
+            // txtPort
+            // 
+            this.txtPort.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtPort.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPort.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPort.Location = new System.Drawing.Point(90, 105);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(138, 19);
+            this.txtPort.TabIndex = 20;
+            this.txtPort.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(23, 105);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(61, 19);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "PORT  : ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(88, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(140, 1);
+            this.panel1.TabIndex = 18;
+            // 
+            // txtIP
+            // 
+            this.txtIP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtIP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtIP.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtIP.Location = new System.Drawing.Point(90, 60);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(138, 19);
+            this.txtIP.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(23, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 19);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "IP  : ";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuit.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnQuit.Location = new System.Drawing.Point(353, 4);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(24, 24);
+            this.btnQuit.TabIndex = 26;
+            this.btnQuit.Text = "X";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textBox6.Location = new System.Drawing.Point(23, 6);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(78, 19);
+            this.textBox6.TabIndex = 25;
+            this.textBox6.Text = "CLIENT";
+            // 
+            // btnSend
+            // 
+            this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSend.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSend.Location = new System.Drawing.Point(283, 270);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(94, 30);
+            this.btnSend.TabIndex = 24;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // backgroundpanel
+            // 
+            this.backgroundpanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.backgroundpanel.Controls.Add(this.txtMessage);
+            this.backgroundpanel.Location = new System.Drawing.Point(23, 173);
+            this.backgroundpanel.Name = "backgroundpanel";
+            this.backgroundpanel.Size = new System.Drawing.Size(354, 94);
+            this.backgroundpanel.TabIndex = 27;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMessage.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.txtMessage.Location = new System.Drawing.Point(3, 1);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(348, 90);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.TabStop = false;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtStatus.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.txtStatus.Location = new System.Drawing.Point(3, 1);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(348, 90);
+            this.txtStatus.TabIndex = 7;
+            this.txtStatus.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel3.Controls.Add(this.txtStatus);
+            this.panel3.Location = new System.Drawing.Point(23, 326);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(354, 94);
+            this.panel3.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(23, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(175, 19);
+            this.textBox3.TabIndex = 29;
+            this.textBox3.Text = "OUTGOING MESSAGE";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Location = new System.Drawing.Point(23, 301);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(175, 19);
+            this.textBox5.TabIndex = 30;
+            this.textBox5.Text = "INCOMMING MESSAGE";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(402, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.backgroundpanel);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Client";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "_client";
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.backgroundpanel.ResumeLayout(false);
+            this.backgroundpanel.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Panel backgroundpanel;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
---
+++ Client/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using SimpleTCP;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private SimpleTcpClient _client;
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            _client = new SimpleTcpClient { StringEncoder = Encoding.UTF8 };
+            _client.DataReceived += Client_DataReceived;
+        }
+
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                txtStatus.Text += e.MessageString;
+            });
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            _client.Connect(txtIP.Text, int.Parse(txtPort.Text));
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            _client.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(3));
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
